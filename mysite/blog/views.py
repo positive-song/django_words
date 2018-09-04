9e00b4a4aba7a583b7eaf96a8c42ee55b59c7bc1
leegusong <orzrzlyo@gmail.com>
2018-09-05 03:50:55 +0900

    단어 추가

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -7,6 +7,16 @@ import random
 def post_list(request):
     df = pd.read_csv('words.csv', encoding='cp949')
     words = df['word']
+
+    # 단어 & 뜻 선택
     word = random.choice(words)
-    word2 = random.choice(words)
-    return render(request, 'blog/post_list.html', {'abc': word, 'word2': word2})
+    meaning = df[words == word]['meaning'].values[0]
+
+    # 다른 보기 만들기
+    other = random.sample(set(df[df['meaning'] != meaning]['meaning']), 3)
+
+    # 진짜 뜻을 추가
+    other.append(meaning)
+    random.shuffle(other)
+
+    return render(request, 'blog/post_list.html', {'word': word, 'definition': other, 'correct': meaning})
