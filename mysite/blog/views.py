70a019af52c608f928f89f6dd79fece61b99e379
LAPTOP-5EEBDO2M\uesr <orzrzlyo@gmail.com>
2018-09-08 16:53:02 +0900

    객관식 만들기

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,5 +8,12 @@ def post_list(request):
     df = pd.read_csv('words.csv', encoding='cp949')
     words = df['word']
     word = random.choice(words)
-    word2 = random.choice(words)
-    return render(request, 'blog/post_list.html', {'abc': word, 'word2': word2})
+
+    meaning = df[words == word]['meaning']
+
+    # 다른 보기 만들기
+    choices = random.sample(set(df[df['meaning'] != meaning]['meaning']), 3)
+    choices.append(meaning)
+    random.shuffle(choices)
+
+    return render(request, 'blog/post_list.html', {'word': word, 'definition': meaning, 'choices': choices})
