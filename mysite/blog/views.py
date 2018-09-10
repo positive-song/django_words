7100bbb5bee82283b9c3739a771bfd2298d1ecc7
LAPTOP-5EEBDO2M\uesr <orzrzlyo@gmail.com>
2018-09-10 23:57:10 +0900

    word_list 생성하여 중복 피하기

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,11 +3,24 @@ import pandas as pd
 import random
 
 
+class AnswerSheet:
+    word_list = []
+
+    def get_words(self, word):
+        self.word_list.append(word)
+
+
 # Create your views here.
 def post_list(request):
     df = pd.read_csv('words.csv', encoding='cp949')
     words = df['word']
     word = random.choice(words)
+
+
+    ## 중복되면 넘어가도록 설정해야함
+
+    answers = AnswerSheet()
+    answers.get_words(word)
 
     meaning = df[words == word]['meaning'].values[0]
 
